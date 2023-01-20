f230da4963bd79b76007c30a7628e98606d96ea6
JuusoLeino <juuso.leino@student.careeria.fi>
2023-01-20 10:05:08 +0200

    Harjoitus 1 Commit*

---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus1/Ajoneuvo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus1
+{
+    class Ajoneuvo
+    {
+        public string Nimi { get; set; }
+        public int Nopeus { get; set; }
+        public int Renkaat { get; set; }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Ajoneuvon tiedot:");
+            Console.WriteLine("-- Nimi: " + Nimi);
+            Console.WriteLine("-- Nopeus: " + Nopeus);
+            Console.WriteLine("-- Renkaat: " + Renkaat);
+        }
+
+        public string ToString()
+        {
+            string merkkijono = "Ajoneuvon tiedot: Nimi: " + Nimi + ". Nopeus: " + Nopeus + "km/h. Renkaat: " + Renkaat + " kpl.";
+            return merkkijono;
+        }
+    }
+}
---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Harjoitus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //auto olio Ajoneuvo -luokasta
+            Ajoneuvo auto = new Ajoneuvo();
+
+            //Alustetaan olion auto tiedot;
+            auto.Nimi = "Lada";
+            auto.Nopeus = 55;
+            auto.Renkaat = 3;
+
+            //tulostetaan auto olion tiedot
+            auto.TulostaData();
+
+            //alustetaan ajoneuvonTiedot muuttuja auto olion tiedoilla jotta ne voi tulostaa
+            string ajoneuvonTiedot = auto.ToString();
+
+            //tulostetaan auto olion tiedot eri tavalla
+            Console.WriteLine(ajoneuvonTiedot);
+
+            //mopo olio Ajoneuvo luokasta
+            Ajoneuvo mopo = new Ajoneuvo();
+
+            //Alustetaan mopo olion tiedot
+            mopo.Nimi = "Kevari";
+            mopo.Nopeus = 80;
+            mopo.Renkaat = 2;
+
+            //tulostetaan mopo olion tiedot kahdella eri tavalla
+            mopo.TulostaData();
+            Console.WriteLine(mopo.ToString());
+        }
+    }
+}
