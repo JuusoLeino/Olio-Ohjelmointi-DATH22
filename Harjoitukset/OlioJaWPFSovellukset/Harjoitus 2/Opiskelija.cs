1099385f2a94480290b63a7a93e327d4d6d45665
JuusoLeino <juuso.leino@student.careeria.fi>
2023-01-20 11:49:02 +0200

    Harjoitus 2 Commit

---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 2/Opiskelija.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_2
+{
+    class Opiskelija
+    {
+        private string Nimi { get; set; }
+        private string OpiskelijaID { get; set; }
+        private int Opintopisteet { get; set; }
+
+        //Luodaan konstruktori
+        public Opiskelija(string _nimi, string _id, int _op)
+        {
+            Nimi = _nimi;
+            OpiskelijaID = _id;
+            Opintopisteet = _op;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Opiskelija tiedot:");
+            Console.WriteLine("-- Nimi: " + Nimi);
+            Console.WriteLine("-- OpiskelijaID: " + OpiskelijaID);
+            Console.WriteLine("-- Opintopisteet: " + Opintopisteet);
+            Console.WriteLine("");
+        }
+
+        public void MuokkaaOpintopisteita(int i)
+        {
+            Opintopisteet += i;
+
+            if(Opintopisteet < 0)
+            {
+                Opintopisteet = 0;
+            }
+        }
+    }
+}
---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //oppilas1 olio Opiskelija luokasta
+            Opiskelija oppilas1 = new Opiskelija("Pekka", "AaA123", 0);
+            Opiskelija oppilas2 = new Opiskelija("Timo", "ThQ325", 0);
+
+            oppilas1.TulostaData();
+            oppilas1.MuokkaaOpintopisteita(15);
+            oppilas1.TulostaData();
+
+            oppilas2.TulostaData();
+            oppilas2.MuokkaaOpintopisteita(20);
+            oppilas2.TulostaData();
+        }
+    }
+}
