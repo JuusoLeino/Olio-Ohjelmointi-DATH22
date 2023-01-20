f969dd33c94c6043b89fac211dcec1e2998b92e1
JuusoLeino <juuso.leino@student.careeria.fi>
2023-01-20 14:36:55 +0200

    Harjoitus 3 (KT) Commit

---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 3 (KT)/Kiuas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_3__KT_
+{
+    class Kiuas
+    {
+        private bool OnOff { get; set; }
+        private int Lämpötila { get; set; }
+        private string Kosteus { get; set; }
+
+        //luodaan konstruktori
+        public Kiuas (bool _onoff, int _lämpötila, string _kosteus)
+        {
+            OnOff = _onoff;
+            Lämpötila = _lämpötila;
+            Kosteus = _kosteus;
+        }
+
+        //metodi jolla data tulostetaan
+        public void TulostaData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Kiuaksen tiedot:");
+            if(OnOff == true)
+            {
+                Console.WriteLine("-- Kiuas on päällä");
+            }
+            else if (OnOff == false)
+            {
+                Console.WriteLine("-- Kiuas on pois päältä");
+            }
+            Console.WriteLine("-- Lämpötila: " + Lämpötila);
+            Console.WriteLine("-- Kosteus: " + Kosteus);
+            Console.WriteLine();
+
+        }
+
+        //metodi jolla muuttaa arvoja
+        public void MuutaArvoja(bool b, int i, string s)
+        {
+            OnOff = b;
+            Lämpötila += i;
+            Kosteus = s;
+        }
+    }
+}
---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 3 (KT)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Harjoitus_3__KT_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //luodaan kiuas1 olio Kiuas luokasta
+            Kiuas kiuas1 = new Kiuas(false, 0, "kuiva");
+
+            //datan tulostus ja muuttaminen
+            kiuas1.TulostaData();
+            kiuas1.MuutaArvoja(true, 15, "hieman kostea");
+            kiuas1.TulostaData();
+            kiuas1.MuutaArvoja(true, 20, "enemmän kostea");
+            kiuas1.TulostaData();
+            kiuas1.MuutaArvoja(false, -35, "kuiva");
+            kiuas1.TulostaData();
+
+        }
+    }
+}
