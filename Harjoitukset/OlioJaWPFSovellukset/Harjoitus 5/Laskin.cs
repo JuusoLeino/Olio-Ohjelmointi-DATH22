3f91840543b420c80a43a6a85e2d79c32237ac5c
JuusoLeino <juuso.leino@student.careeria.fi>
2023-01-23 11:13:13 +0200

    Harjotus 5 Commit

---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 5/Laskin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_5
+{
+    static class Laskin
+    {
+        public static float Summa(float a, float b)
+        {
+            float summa = a + b;
+            return summa;
+        }
+
+        public static float Erotus(float a, float b)
+        {
+            float erotus = a - b;
+            return erotus;
+        }
+
+        public static float Tulos(float a, float b)
+        {
+            float tulo = a * b;
+            return tulo;
+        }
+
+        public static float Osamäärä(float a, float b)
+        {
+            float osamäärä = a / b;
+            return osamäärä;
+        }
+    }
+}
---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Harjoitus_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0.6f;
+            float b = 2.3f;
+
+            float summa = Laskin.Summa(a, b);
+            float erotus = Laskin.Erotus(a, b);
+            float tulo = Laskin.Tulos(a, b);
+            float osamäärä = Laskin.Osamäärä(a, b);
+
+            Console.WriteLine(summa);
+            Console.WriteLine(erotus);
+            Console.WriteLine(tulo);
+            Console.WriteLine(osamäärä);
+        }
+    }
+}
