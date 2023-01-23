0b48e16080721416e0e50c4bee4367be86f02bf2
JuusoLeino <juuso.leino@student.careeria.fi>
2023-01-23 15:19:57 +0200

    Harjoitus 7 Commit

---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 7/Hissi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_7
+{
+    class Hissi
+    {
+        private int hissikerros;
+
+        public int HissiKerros
+        {
+          get => hissikerros;
+          set
+          {
+            if (value < 1)
+            {
+                Console.WriteLine("Virheellinen luku. Kerrokset menevät 1-6. Anna uusi kerros: ");
+                value = 1;
+            }
+            else if (value > 6)
+            {
+                Console.WriteLine("Virheellinen luku. Kerrokset menevät 1-6. Anna uusi kerros: ");
+                value = 1;
+            }
+
+
+            Console.WriteLine("\nHissi on kerroksessa: " + value);
+            hissikerros = value;
+          }
+        }
+    }
+}
---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Harjoitus_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uusiKerros;
+            Hissi hissi = new Hissi();
+            Console.WriteLine("Olet nyt hississä pääset ulos syöttämällä \"poistu\"");
+
+            while (true)
+            {
+                Console.WriteLine("Anna hissi kerros (1-6) > ");
+                string luettuArvo = Console.ReadLine();
+
+                if (luettuArvo.Equals("poistu"))
+                {
+                    break;
+                }
+
+                bool result = int.TryParse(luettuArvo, out uusiKerros);
+
+                if (result)
+                {
+                    hissi.HissiKerros = uusiKerros;
+                }
+            }
+        }
+    }
+}
