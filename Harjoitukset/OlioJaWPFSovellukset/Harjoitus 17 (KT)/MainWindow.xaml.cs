ce69c42fcf9b554f011c98a4e1ca51a866684e84
JuusoLeino <juuso.leino@student.careeria.fi>
2023-01-30 14:46:22 +0200

    Harjoitus 17 & 18 Commit

---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 17 (KT)/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus_17__KT_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double lämpötila;
+        private bool keittiöValot = false;
+        private bool olohuoneValot = false;
+        private bool ovenLukko = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_ovenlukko_Click(object sender, RoutedEventArgs e)
+        {
+            if(ovenLukko == false)
+            {
+                ovenLukko = true;
+                btn_ovenlukko.Content = "Kiinni";
+            }else if (ovenLukko == true)
+            {
+                ovenLukko = false;
+                btn_ovenlukko.Content = "Auki";
+            }
+        }
+
+        private void Btn_keittiövalot_Click(object sender, RoutedEventArgs e)
+        {
+            if (keittiöValot == false)
+            {
+                keittiöValot = true;
+                btn_keittiövalot.Content = "Päällä";
+            }
+            else if (keittiöValot == true)
+            {
+                keittiöValot = false;
+                btn_keittiövalot.Content = "Pois Päältä";
+            }
+        }
+
+        private void Btn_olohuonevalot_Click(object sender, RoutedEventArgs e)
+        {
+            if (olohuoneValot == false)
+            {
+                olohuoneValot = true;
+                btn_olohuonevalot.Content = "Päällä";
+            }
+            else if (olohuoneValot == true)
+            {
+                olohuoneValot = false;
+                btn_olohuonevalot.Content = "Pois Päältä";
+            }
+        }
+
+        private void Sldr_Lämpötila_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lämpötila = sldr_Lämpötila.Value;
+        }
+    }
+}
---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitus 18 (KT)/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus_18__KT_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int luku1;
+        private int luku2;
+        private int laskuToimitus;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_summa_Click(object sender, RoutedEventArgs e)
+        {
+            luku1 = int.Parse(txt_luku1.Text);
+            luku2 = int.Parse(txt_luku2.Text);
+
+            laskuToimitus = luku1 + luku2;
+
+            txt_laskutoimitus.Text = laskuToimitus.ToString();
+        }
+
+        private void Btn_erotus_Click(object sender, RoutedEventArgs e)
+        {
+            luku1 = int.Parse(txt_luku1.Text);
+            luku2 = int.Parse(txt_luku2.Text);
+
+            laskuToimitus = luku1 - luku2;
+
+            txt_laskutoimitus.Text = laskuToimitus.ToString();
+        }
+
+        private void Btn_tulo_Click(object sender, RoutedEventArgs e)
+        {
+            luku1 = int.Parse(txt_luku1.Text);
+            luku2 = int.Parse(txt_luku2.Text);
+
+            laskuToimitus = luku1 * luku2;
+
+            txt_laskutoimitus.Text = laskuToimitus.ToString();
+        }
+
+        private void Btn_osamäärä_Click(object sender, RoutedEventArgs e)
+        {
+            luku1 = int.Parse(txt_luku1.Text);
+            luku2 = int.Parse(txt_luku2.Text);
+
+            laskuToimitus = luku1 / luku2;
+
+            txt_laskutoimitus.Text = laskuToimitus.ToString();
+        }
+    }
+}
