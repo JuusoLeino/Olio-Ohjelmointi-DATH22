a8cd7f217a79fdf843b3ba7bd0c8967bf441a9ba
JuusoLeino <juuso.leino@student.careeria.fi>
2023-01-30 11:15:11 +0200

    Harjoitus 15 & 16 Commit

---
+++ Harjoitukset/OlioJaWPFSovellukset/Hajroitus 15/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hajroitus_15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int henkilöAutot = 0;
+        private int kuormaAutot = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txt_henkiköauto.Text = henkilöAutot.ToString();
+            txt_kuormaauto.Text = kuormaAutot.ToString();
+        }
+
+        private void Btn_henkilöauto_Click(object sender, RoutedEventArgs e)
+        {
+            henkilöAutot++;
+            txt_henkiköauto.Text = henkilöAutot.ToString();
+        }
+
+        private void Btn_kuormaauto_Click(object sender, RoutedEventArgs e)
+        {
+            kuormaAutot++;
+            txt_kuormaauto.Text = kuormaAutot.ToString();
+        }
+    }
+}
---
+++ Harjoitukset/OlioJaWPFSovellukset/Harjoitsu 16/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitsu_16
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double km;
+        private double maili;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_kmtomile_Click(object sender, RoutedEventArgs e)
+        {
+            km = double.Parse(txt_kilometri.Text);
+            maili = km * 0.621371;
+            txt_mailit.Text = maili.ToString();
+        }
+
+        private void Btn_miletokm_Click(object sender, RoutedEventArgs e)
+        {
+            maili = double.Parse(txt_mailit.Text);
+            km = maili * 1.609344;
+            txt_kilometri.Text = km.ToString();
+        }
+    }
+}
